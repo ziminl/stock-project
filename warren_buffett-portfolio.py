371a04289b83b9ee680cbaf098af657ce340d9b8
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-06-16 13:06:49 +0900

    Create warren_buffett-portfolio.py

---
+++ warren_buffett-portfolio.py
@@ -0,0 +1,5 @@
+##https://stockcircle.com/portfolio/warren-buffett
+##https://www.reddit.com/r/investing/comments/51sf09/api_to_get_info_for_portfolios_of_famous/
+##https://www.gurufocus.com/guru/warren%2Bbuffett/current-portfolio/portfolio?view=table
+
+##idea : track with api or selenium
